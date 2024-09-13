970bc7912172f9abdbc38c9a1e90381a94e5d627
DIYables <134663277+DIYables@users.noreply.github.com>
2024-09-13 10:15:42 +0900

    Add files via upload

---
+++ build/lib/DIYables_MicroPython_Ultrasonic_Sensor/__init__.py
@@ -0,0 +1,2 @@
+# DIYables_MicroPython_Ultrasonic_Sensor/__init__.py
+from .DIYables_MicroPython_Ultrasonic_Sensor import UltrasonicSensor
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DIYables_MicroPython_Ultrasonic_Sensor",
-    version="0.1.1",
+    version="0.1.2",
     description="MicroPython Library for Ultrasonic Sensor, created by DIYables",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
